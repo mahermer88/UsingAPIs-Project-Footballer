02e8830717036c949fc71effcfabd76dcf7798dc
Maher Mirali <51765604+mahermer88@users.noreply.github.com>
2021-04-05 15:36:54 +0200

    Update fetch-matches-up-day.js

--- src/handlers/fetch-matches-up-day.js
+++ src/handlers/fetch-matches-up-day.js
@@ -4,13 +4,9 @@ import { renderError } from "../views/render-error.js";
 
 export async function filterMatchesUpDay(code, day) {
   try {
-    console.log(code, day)
     const url = `https://api.football-data.org/v2/competitions/${code}/matches/?matchday=${day}`;
-    console.log(url)
     const data = await fetchData(url);
-    console.log(data)
     const matchesList = await data.matches;
-     console.log(matchesList)
     renderMatch(matchesList);
   } catch (error) {
     renderError(error);
